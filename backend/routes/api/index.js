9fe69a592bd8213262d980b5e375b8d59e4eaa2a
LukeConnors <LukeTConnors@yahoo.com>
2023-05-24 20:01:47 -0500

    added spot-images routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,6 +8,7 @@ const usersRouter = require('./users.js')
 const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
 const bookingsRouter = require('./bookings.js')
+const spotImagesRouter = require('./spot-images.js')
 
 router.use(restoreUser);
 router.use('/session', sessionRouter);
@@ -15,6 +16,7 @@ router.use('/users', usersRouter)
 router.use('/spots', spotsRouter)
 router.use('/reviews', reviewsRouter)
 router.use('/bookings', bookingsRouter)
+router.use('/spot-images', spotImagesRouter)
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {Spot, User, Image, Booking} = require('../../db/models')
+const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+router.delete('/:imageId', async (req, res, next) => {
+    const image = await Image.findByPk(req.params.imageId)
+    if(image.imageableType !== 'Spot'){
+        next({
+            status: 404,
+            message: "Spot image couldn't be found"
+        })
+    }
+    await image.destroy()
+    res.json({message: "Successfully deleted"})
+})
+
+module.exports = router;
