accf543d0317ab53fa011c4e107cfba090ab517b
LukeConnors <LukeTConnors@yahoo.com>
2023-05-26 06:28:48 -0500

    still working on it

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -6,16 +6,45 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
 
 router.delete('/:imageId', requireAuth, async (req, res, next) => {
-    const image = await Image.findByPk(req.params.imageId);
-    const user = req.user;
-    const review = await Review.findByPk(image.imageableId)
-    if(!image){
-        next({
-            status: 404,
-            message: "Review image couldn't be found"
-        })
-        return
-    }
+//     const image = await Image.findByPk(req.params.imageId);
+//     const user = req.user;
+//     const review = await Review.findByPk(image.imageableId)
+//     if(!image){
+//         next({
+//             status: 404,
+//             message: "Review image couldn't be found"
+//         })
+//         return
+//     }
+//     if(review.userId !== user.id){
+//         next({
+//             status: 403,
+//             message: "Forbidden"
+//         })
+//         return
+//     }
+//     if(image.imageableType !== 'Review'){
+//         next({
+//             status: 400,
+//             message: "The image you are trying to delete does not belong to a Review!"
+//         })
+//         return
+//     } else if(image.imageableType === 'Review'){
+//         await image.destroy()
+//         res.json({message: "Successfully deleted"})
+//     }
+// })
+const image = await Image.findByPk(req.params.imageId)
+const user = req.user
+const review = await Review.findByPk(image.imageableId)
+if(!image){
+    next({
+        status: 404,
+        message: "Spot image couldn't be found"
+    })
+    return
+}
+if(image.imageableType === 'Review'){
     if(review.userId !== user.id){
         next({
             status: 403,
@@ -23,17 +52,18 @@ router.delete('/:imageId', requireAuth, async (req, res, next) => {
         })
         return
     }
-    if(image.imageableType !== 'Review'){
-        next({
-            status: 400,
-            message: "The image you are trying to delete does not belong to a Review!"
-        })
-        return
-    } else if(image.imageableType === 'Review'){
-        await image.destroy()
-        res.json({message: "Successfully deleted"})
-    }
+}
+if(image.imageableType !== 'Review'){
+    next({
+        status: 400,
+        message: "The image you are trying to delete does not belong to a Review!"
+    })
+    return
+}
+await image.destroy()
+res.json({message: "Successfully deleted"})
 })
+
 
 
 module.exports = router;
