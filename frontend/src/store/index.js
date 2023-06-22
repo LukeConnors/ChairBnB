b5d7292012a94df5571d4994a5bf1c92f99499dc
LukeConnors <LukeTConnors@yahoo.com>
2023-06-22 18:10:30 -0500

    Auth-me Phase 0 complete

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+
 const rootReducer = combineReducers({
   // add reducer functions here
 });
@@ -12,12 +13,13 @@ if (process.env.NODE_ENV === "production") {
 } else {
   const logger = require("redux-logger").default;
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
 
 export default configureStore;
