48fbf1efdc1a7fe0e20d7dd30c4f6316411f0d4c
LukeConnors <LukeTConnors@yahoo.com>
2023-07-17 08:35:40 -0500

    refactored spotDetails

--- frontend/src/components/Spots/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails.js
@@ -64,7 +64,7 @@ const SpotDetails = () => {
     }
   }
 
-  if (!user || user?.id === spot?.ownerId) {
+  // if (!user || user?.id === spot?.ownerId) {
 
     return (
       <div className="details">
@@ -137,81 +137,82 @@ const SpotDetails = () => {
       </div>
     );
 
-  } else if(user){
+  // }
+  // else if(user){
 
-    return (
-      <div className="details">
-        <h1>{name}</h1>
-        <h2 className="location">{city}, {state}, {country}</h2>
-        <div className="img-container">
-          {SpotImages && SpotImages.length > 0 ? (
-            <>
-              <img className='main-img' key={SpotImages[0]?.id} src={SpotImages[0]?.url} alt={name} />
-              <div className="small-images">
-                {SpotImages?.slice(1).map(image => (
-                  <img className='secondary-img' key={image?.id} src={image?.url} alt={name} />
-                ))}
-              </div>
-            </>
-          ) : (
-            <p>No images available</p>
-          )}
-        </div>
+  //   return (
+  //     <div className="details">
+  //       <h1>{name}</h1>
+  //       <h2 className="location">{city}, {state}, {country}</h2>
+  //       <div className="img-container">
+  //         {SpotImages && SpotImages.length > 0 ? (
+  //           <>
+  //             <img className='main-img' key={SpotImages[0]?.id} src={SpotImages[0]?.url} alt={name} />
+  //             <div className="small-images">
+  //               {SpotImages?.slice(1).map(image => (
+  //                 <img className='secondary-img' key={image?.id} src={image?.url} alt={name} />
+  //               ))}
+  //             </div>
+  //           </>
+  //         ) : (
+  //           <p>No images available</p>
+  //         )}
+  //       </div>
 
-        <div className="chair-details">
-          <div className="chair-info">
-            {Owner && <p className="host">Hosted by: {Owner?.firstName} {Owner?.lastName}</p>}
-            <p className="des">{description}</p>
-          </div>
+  //       <div className="chair-details">
+  //         <div className="chair-info">
+  //           {Owner && <p className="host">Hosted by: {Owner?.firstName} {Owner?.lastName}</p>}
+  //           <p className="des">{description}</p>
+  //         </div>
 
-          <div className="star-cont">
-            <div className="boxy">
-              <div className="price">
-                <p>${price} per day</p>
-              </div>
-              <div className="rating-div">
-              <i className="fa-solid fa-star" style={{ color: "#b00c0c" }}></i>
-            <p>{!avgStarRating ? 'New' : avgStarRating?.toFixed(2)}</p>
-                {/* <p className="dot">·</p> */}
-                <p className="rev-count">{handleReviewCount()}</p>
-              </div>
-            </div>
-            <div>
-              {user && user.id === spot.ownerId ? (
-                <div>
-                  <button className="ed-spot" onClick={handleEditSpot}>
-                    Edit Spot
-                  </button>
-                  <button className="del-spot" onClick={(e) => {
-                    e.preventDefault();
-                    setModal('deleteSpot')
-                  }}>
-                    Delete Spot
-                  </button>
-                </div>
-              ) : (
-                <div className="jerry">
-                  <button className='reserve' onClick={handleReserve}>Reserve</button>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
+  //         <div className="star-cont">
+  //           <div className="boxy">
+  //             <div className="price">
+  //               <p>${price} per day</p>
+  //             </div>
+  //             <div className="rating-div">
+  //             <i className="fa-solid fa-star" style={{ color: "#b00c0c" }}></i>
+  //           <p>{!avgStarRating ? 'New' : avgStarRating?.toFixed(2)}</p>
+  //               {/* <p className="dot">·</p> */}
+  //               <p className="rev-count">{handleReviewCount()}</p>
+  //             </div>
+  //           </div>
+  //           <div>
+  //             {user && user.id === spot.ownerId ? (
+  //               <div>
+  //                 <button className="ed-spot" onClick={handleEditSpot}>
+  //                   Edit Spot
+  //                 </button>
+  //                 <button className="del-spot" onClick={(e) => {
+  //                   e.preventDefault();
+  //                   setModal('deleteSpot')
+  //                 }}>
+  //                   Delete Spot
+  //                 </button>
+  //               </div>
+  //             ) : (
+  //               <div className="jerry">
+  //                 <button className='reserve' onClick={handleReserve}>Reserve</button>
+  //               </div>
+  //             )}
+  //           </div>
+  //         </div>
+  //       </div>
 
 
-        <div className="reviews">
-          <div className="rev-info">
-          <i className="fa-solid fa-star" style={{ color: "#b00c0c" }}></i>
-            <p>{!avgStarRating ? 'New' : avgStarRating?.toFixed(2)}</p>
-            {/* <p className="dot">·</p> */}
-            <p className="rev-count"> {handleReviewCount()}</p>
-          </div>
-          <SpotReviews />
-        </div>
-      </div>
-    );
+  //       <div className="reviews">
+  //         <div className="rev-info">
+  //         <i className="fa-solid fa-star" style={{ color: "#b00c0c" }}></i>
+  //           <p>{!avgStarRating ? 'New' : avgStarRating?.toFixed(2)}</p>
+  //           {/* <p className="dot">·</p> */}
+  //           <p className="rev-count"> {handleReviewCount()}</p>
+  //         </div>
+  //         <SpotReviews />
+  //       </div>
+  //     </div>
+  //   );
 
-  }
+  // }
 }
 
 export default SpotDetails
